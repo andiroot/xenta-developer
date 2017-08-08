20c9b0e7f55b1c00b2290c23eff902f0cf9a8c5d
Dindin Hernawan <dindin_hernawan@yahoo.com>
2017-08-08 22:12:09 +0700

    Create sample-header.sh

---
+++ tim-pemaketan/membuat-paket/sample-header.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Aplications : build-iso "Script Untuk Membuat ISO dari filesistem remastering sementara 
+#               untuk tujuan pengetesan "
+# Copyright   : GNU GPLv3 
+# Versi       : 0.1
+# Author      : Dindin Hernawan <root@dev.xentaos.org>
+#
